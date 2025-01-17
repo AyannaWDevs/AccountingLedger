c1b996c19dafac7bed4f2e6c8eb83356dd9c11f9
poteatc <poteatc@outlook.com>
2025-01-17 11:14:18 -0500

    Refactored for loops to use streams to print transactions with toString

--- pluralsight-capstone-1/src/main/java/com/pluralsight/capstone/LedgerLogic.java
+++ pluralsight-capstone-1/src/main/java/com/pluralsight/capstone/LedgerLogic.java
@@ -12,18 +12,19 @@ public class LedgerLogic {
     }
 
     public static void displayEntries() {
-        for (Transaction transaction : transactions) {
-            System.out.println(transaction.getDate() + "|" + transaction.getTime() + "|" + transaction.getDescription() + "|" + transaction.getVendor() + "|" + transaction.getAmount() + "\n");
-
-        }
+        transactions.forEach(System.out::println);
+//        for (Transaction transaction : transactions) {
+//            System.out.println(transaction);
+//        }
     }
 
     public static void displayDeposits() {
-        for (Transaction transaction : transactions) {
-            if (transaction.getAmount() >= 0) {
-                System.out.println(transaction.getDate() + "|" + transaction.getTime() + "|" + transaction.getDescription() + "|" + transaction.getVendor() + "|" + transaction.getAmount() + "\n");
-            }
-        }
+        transactions.stream().filter(t -> t.getAmount() >= 0).forEach(System.out::println);
+//        for (Transaction transaction : transactions) {
+//            if (transaction.getAmount() >= 0) {
+//                System.out.println(transaction.getDate() + "|" + transaction.getTime() + "|" + transaction.getDescription() + "|" + transaction.getVendor() + "|" + transaction.getAmount() + "\n");
+//            }
+//        }
     }
 
     public static void displayPayments() {
