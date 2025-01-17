43312d145f9b26601275bc6cc045674458627dd3
adrianvences9820 <adrianvences9820@gmail.com>
2025-01-17 10:18:18 -0500

    fixed ledger screen loop

--- pluralsight-capstone-1/src/main/java/com/pluralsight/capstone/LedgerApplication.java
+++ pluralsight-capstone-1/src/main/java/com/pluralsight/capstone/LedgerApplication.java
@@ -11,7 +11,7 @@ public class LedgerApplication {
         /* Read and write a text file
            Format the file I/OII
            Have methods for each screen
-            */
+        */
         ReadAndWrite.readToFile();
         boolean done = false;
         while(!done) {
@@ -50,37 +50,41 @@ public class LedgerApplication {
     }
 
     public static void displayLedgerScreen(Scanner scanner) {
-        String header = String.format("Date", "Time", "Description", "Vendor", "Amount");
-        System.out.println("""
-                Welcome to the Ledger Screen
-                =====================================================
-                Enter:
-                'A' to display all entries
-                'D' to display all deposits
-                'P' to display all payments
-                'R' to enter the Reports Screen to perform a custom search
-                'H' to return to the Home Screen
-                """);
-        String input = scanner.nextLine().toUpperCase().trim(); //take user input
-        switch (input) {
-            case "A": //prompt user for deposit info and save to csv file
-                LedgerLogic.displayEntries(); // displays all entries
-                break;
-            case "D":
-                LedgerLogic.displayDeposits(); // displays all deposits
-                break;
-            case "P":
-                LedgerLogic.displayPayments(); //displays all payments
-                break;
-            case "R":
-                reportsScreen(); //goes to reports screen
-                break;
-            case "H":
-                System.out.println("Now returning to home screen...");
-                break;
-            default:
-                System.out.println("Invalid input. Please enter A, D, P, R, or H");
-                displayLedgerScreen(scanner);
+        boolean loopFlag = true;
+        while(loopFlag) {
+            String header = String.format("Date", "Time", "Description", "Vendor", "Amount");
+            System.out.println("""
+                    Welcome to the Ledger Screen
+                    =====================================================
+                    Enter:
+                    'A' to display all entries
+                    'D' to display all deposits
+                    'P' to display all payments
+                    'R' to enter the Reports Screen to perform a custom search
+                    'H' to return to the Home Screen
+                    """);
+            String input = scanner.nextLine().toUpperCase().trim(); //take user input
+            switch (input) {
+                case "A": //prompt user for deposit info and save to csv file
+                    LedgerLogic.displayEntries(); // displays all entries
+                    break;
+                case "D":
+                    LedgerLogic.displayDeposits(); // displays all deposits
+                    break;
+                case "P":
+                    LedgerLogic.displayPayments(); //displays all payments
+                    break;
+                case "R":
+                    reportsScreen(); //goes to reports screen
+                    break;
+                case "H":
+                    System.out.println("Now returning to home screen...");
+                    loopFlag = false;
+                    break;
+                default:
+                    System.out.println("Invalid input. Please enter A, D, P, R, or H");
+                    displayLedgerScreen(scanner);
+            }
         }
     }
 
@@ -94,7 +98,7 @@ public class LedgerApplication {
                     3) Year to Date
                     4) Previous Year
                     5) Search by Vendor
-                    0) Return to Reports Screen
+                    0) Return to Ledger Screen
                     """);
             Scanner scanner = new Scanner(System.in);
             int choice = scanner.nextInt();
